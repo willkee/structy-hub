5bf00374c013dcd0290d7820831b98e5c80b1091
Will Kee <93359204+willkee@users.noreply.github.com>
2024-02-01 17:48:42 -0700

    Solved 'zipper lists' (.py)

--- zipper_lists/zipper_lists.py
+++ zipper_lists/zipper_lists.py
@@ -15,14 +15,12 @@ def zipper_lists(head_1, head_2):
   while (current1 is not None and current2 is not None):
     if count % 2 == 0:
       # even number ( get from list 2 )
-      print(tail.val, tail.next.val, 'even')
       tail.next = current2
       tail = current2
       
       current2 = current2.next
     else:
       # odd number ( get from list 1 )
-      print(tail.val, tail.next.val, 'odd')
       tail.next = current1
       tail = current1
       
@@ -37,4 +35,3 @@ def zipper_lists(head_1, head_2):
   
   return head_1
 ​
-  
