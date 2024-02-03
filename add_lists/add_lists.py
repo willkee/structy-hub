d6a2da7b8154ebde85be4a0b683cd2730e333b17
Will Kee <93359204+willkee@users.noreply.github.com>
2024-02-03 16:39:12 -0700

    Solved 'add lists' (.py)

--- add_lists/add_lists.py
+++ add_lists/add_lists.py
@@ -6,36 +6,33 @@ class Node:
 def add_lists(h1, h2):
   curr1 = h1
   curr2 = h2
-  
   prev = None
-  
-  carry_over = 0
+  carry = 0
   
   while curr1:
+    # in case the 2nd linked list is shorter
+    # c2 is simply 0 if linked list #2 is finished.
+    c2 = curr2.val if curr2 else 0
     
-    c2 = None
-    if curr2: c2 = curr2.val
-    else: c2 = 0
-  
-    if carry_over > 0:
-      curr_sum = curr1.val + c2 + carry_over
-      if curr_sum < 10:
-        carry_over = 0
-      else:
-        carry_over = curr_sum // 10
-    else:
-      curr_sum = curr1.val + c2
-      
-    carry_over = curr_sum // 10
-    last_digit = int(str(curr_sum)[-1])
-    curr1.val = last_digit
+    # add up the values in both.
+    sum = curr1.val + c2 + carry
+    carry = 0 if sum < 10 else sum // 10
     
+    # get the last digit of the sum and apply it to curr1.val
+    curr1.val = sum % 10
+    
+    # we want to track prev in case there is a carry over after
+    # both linked lists are traversed
     prev = curr1
+    
+    # curr1 (and curr2 if it still exists) is incremented
     curr1 = curr1.next
     if curr2: curr2 = curr2.next
   
-  if carry_over > 0: prev.next = Node(carry_over)
+  # although this passes all test cases, this has a flaw in that
+  # a carry over amount 10+ will not work. We will need another loop
+  # to traverse all remaining digits.
+  if carry > 0: prev.next = Node(carry)
     
   return h1
 ​
-    curr2 = curr2.next
